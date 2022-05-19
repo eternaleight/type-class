686ccd51a67c4f6bf60f4f117437bdebf2307769
eternaleight <eternaleight0@gmail.com>
2022-05-20 02:36:30 +0900

    feat(design): singleton patern

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,46 +2,56 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   class Dog {
-    static info = 'dogクラスのクラス変数です'
+   private static instance: Dog
+    static info = 'Dogクラスのstatic変数です'
     age: number
     breed: string
     item: string
-    constructor(age: number, breed: string, item = 'uuuu') {
-      this.age = age
-      this.breed = breed
-      this.item = item
+    private constructor(_age: number, _breed: string, _item = 'uuuu') {
+      this.age = _age
+      this.breed = _breed
+      this.item = _item
     }
     getRelativeAge(this: Dog): number | string {
       return this.age * 7 + ' '
     }
-  }
-  class Box extends Dog {
-    constructor(age: number, breed: string, item?: string) {
-      super(age, breed, item)
-    }
-    getRelativeAge(this: Dog): number | string {
-      return this.age * 7
+    static getInstance() {
+      if (Dog.instance) return Dog.instance
+      Dog.instance = new Dog(3, '柴犬', 'gorila')
+      return Dog.instance
     }
   }
+  const animals = Dog.getInstance()
+  console.log(animals)
+  // class Box extends Dog {
+  //   constructor(age: number, breed: string, item?: string) {
+  //     super(age, breed, item)
+  //   }
+  //   getRelativeAge(this: Dog): number | string {
+  //     return this.age * 7
+  //   }
+  // }
 
-  const dog = new Dog(3, '柴犬', 'gorila')
-  const box = new Box(4, 'セントバーナード', 'パレス')
+  // const dog = new Dog(3, '柴犬', 'gorila')
+  // const box = new Box(4, 'セントバーナード', 'パレス')
 
   return (
     <div>
       <p>
-        {`${dog.age}歳 ${dog.breed} ${
-          dog.item
-        } 犬年齢${dog.getRelativeAge()}歳`}
-        <br />
-        {`${Dog.info}`}
-      </p>
-      <p>
-        {`${box.age}歳 ${box.breed} ${
-          box.item
-        } 犬年齢${box.getRelativeAge()}歳`}
-        <br />
-        {`${Box.info}`}
+        {Dog.info} <br />
+        {animals.age + animals.breed + animals.item}
+        {/* {`${dog.age}歳 ${dog.breed} ${ */}
+        {/*   dog.item */}
+        {/* } 犬年齢${dog.getRelativeAge()}歳`} */}
+        {/* <br /> */}
+        {/* {`${Dog.info}`} */}
+        {/* </p> */}
+        {/* <p> */}
+        {/* {`${box.age}歳 ${box.breed} ${ */}
+        {/*   box.item */}
+        {/* } 犬年齢${box.getRelativeAge()}歳`} */}
+        {/* <br /> */}
+        {/* {`${Box.info}`} */}
       </p>
     </div>
   )
